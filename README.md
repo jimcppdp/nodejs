3a495a5bfcde473604f7e1a3f7716a6462049c3c
jimcppdp <jimcppdp@gmail.com>
2019-07-01 23:51:43 +0800

    Add redux-logger test

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nodejs
 nodejs experiments
+
+support  
+
+redux_and_logger  
+  based on https://www.valentinog.com/blog/redux/
--- redux_and_logger/src/js/store/index.js
+++ redux_and_logger/src/js/store/index.js
@@ -1,5 +1,12 @@
 // src/js/store/index.js
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from "../reducers/index";
-const store = createStore(rootReducer);
+
+// Logger with default options
+import logger from 'redux-logger'
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(logger)
+);
 export default store;
